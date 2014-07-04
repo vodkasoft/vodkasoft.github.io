95910e08e92ac2df30a36402324de222102a3b0d
Albin Arias Juárez <alarias@ic-itcr.ac.cr>
2014-07-04 10:29:20 -0600

    Add page

--- scrumagile/js/sysadmin.js
+++
@@ -1,8 +0,0 @@
-function sysadminpage(){
-	document.getElementById("UserNameSpan").innerHTML = localStorage.getItem("Name")+" "+localStorage.getItem("LastName");
-	document.getElementById("UserRoleSpan").innerHTML = "<b><i>Sysadmin</i></b>";
-}
-
-function crearSession(){
-	localStorage.clear();
-}
