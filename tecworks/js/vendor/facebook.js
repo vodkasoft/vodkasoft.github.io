041cfc19954aa57b35e158efdb706cf80eb6c95a
Albin Arias Juárez <alarias@ic-itcr.ac.cr>
2014-07-23 21:35:54 -0600

    Fix facebook function

--- tecworks/js/vendor/facebook.js
+++ tecworks/js/vendor/facebook.js
@@ -31,15 +31,8 @@
   }
 
   FB.login(function(response) {
-  if (response.status === 'connected') {
-    alert("connected");
-  } else if (response.status === 'not_authorized') {
-    // The person is logged into Facebook, but not your app.
-  } else {
-    // The person is not logged into Facebook, so we're not sure if
-    // they are logged into this app or not.
-  }
-});
+   console.log(response);
+ }, {scope: 'user_friends email'});
 
   window.fbAsyncInit = function() {
   FB.init({
