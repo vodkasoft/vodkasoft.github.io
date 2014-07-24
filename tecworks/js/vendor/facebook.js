60820b887ead339c47e2e80717b33c6c0248b1d8
Albin Arias Juárez <alarias@ic-itcr.ac.cr>
2014-07-23 21:33:36 -0600

    Update login function

--- tecworks/js/vendor/facebook.js
+++ tecworks/js/vendor/facebook.js
@@ -30,6 +30,17 @@
     });
   }
 
+  FB.login(function(response) {
+  if (response.status === 'connected') {
+    alert("connected");
+  } else if (response.status === 'not_authorized') {
+    // The person is logged into Facebook, but not your app.
+  } else {
+    // The person is not logged into Facebook, so we're not sure if
+    // they are logged into this app or not.
+  }
+});
+
   window.fbAsyncInit = function() {
   FB.init({
     appId      : '255439164661872',
